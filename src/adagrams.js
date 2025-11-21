db2a20228afdb22dcce12c3ad3e3ac1c8f708fd5
mayskii <94030074+mayskii@users.noreply.github.com>
2025-11-21 10:17:00 -0600

    add wave_3 (#3)

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const LETTER_SCORE = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10
+};
+
 export const drawLetters = () => {
   let hand = [];
 
@@ -67,7 +96,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+
+  if (!word) return 0;
+  let score = 0;
+
+  // calculate the score for the word
+  for (const letter of word.toUpperCase()){
+    score += LETTER_SCORE[letter];
+  }
+  return word.length >= 7 ? score + 8 : score;
 };
 
 export const highestScoreFrom = (words) => {
