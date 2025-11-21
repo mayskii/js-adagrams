e7cf0e33b5b4faea6cd2847fe36bf2d72548ae4b
mayskii <94030074+mayskii@users.noreply.github.com>
2025-11-20 20:29:23 -0600

    add wave_1 (#1)

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,47 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let hand = [];
+
+  const remainingLetters = { ...LETTER_POOL };
+  const letters = Object.keys(LETTER_POOL);
+
+  while (hand.length < 10) {
+    const randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    if (remainingLetters[randomLetter] > 0) {
+      hand.push(randomLetter);
+      remainingLetters[randomLetter]--;
+    }
+  }
+
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
